aec949d0e233344ad40dfa3b9c8682d2b2609508
João Paulo Vicentini Wanderley <joaowanderley@ucl.br>
2019-07-18 15:34:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import m from "mithril";
+
+export default class App {
+  view() {
+    return (
+      <div id="container">
+        <img src="assets/logo.png" width="100" height="100" />
+        <h1>Mithril App</h1>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import m from "mithril";
+
+import App from "./App";
+import "./index.scss";
+
+m.render(document.body, <App />);
